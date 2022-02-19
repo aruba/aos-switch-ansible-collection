6811258eac600381d4370810aa552c6615bd153b
E.S. Rosenberg a.k.a. Keeper of the Keys <es.rosenberg+openu@gmail.com>
2022-02-20 01:10:30 +0200

    Fixes an issue where a binary string is returned causing ansible to crash. Thanks to @bcoca and @mackerman from #ansible on libera.chat :)

--- plugins/modules/arubaoss_file_transfer.py
+++ plugins/modules/arubaoss_file_transfer.py
@@ -89,6 +89,7 @@ EXAMPLES = '''
 '''
 
 from ansible.module_utils.basic import AnsibleModule # NOQA
+from ansible.module_utils.common.text.converters import to_text
 from ansible_collections.arubanetworks.aos_switch.plugins.module_utils.arubaoss import run_commands # NOQA
 from ansible_collections.arubanetworks.aos_switch.plugins.module_utils.arubaoss import arubaoss_argument_spec # NOQA
 from ansible_collections.arubanetworks.aos_switch.plugins.module_utils.arubaoss import get_config # NOQA
@@ -134,7 +135,7 @@ def transfer(module):
     if params['boot_image']:
         data['boot_image'] = params['boot_image']
     result = run_commands(module, url, data, 'POST')
-    message = result.get('body') or None
+    message = to_text(result.get('body')) or None
     if message and 'Another download is in progress' in message:
         result = {'changed': False}
         result['failed'] = True
