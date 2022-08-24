4f69aea8dde3ea04ac49f4d321ff9e9ada5f79c3
Alexis La Goutte <alexis.lagoutte@gmail.com>
2022-08-24 17:34:59 +0200

    aaa_accounting: fix validates-module module-invalid-version-added
    arubaoss_aaa_accounting.py:0:0: module-invalid-version-added: DOCUMENTATION: version_added ('2.4') is not a valid collection version (see specification at https://semver.org/): invalid semantic version '2.4'

--- plugins/modules/arubaoss_aaa_accounting.py
+++ plugins/modules/arubaoss_aaa_accounting.py
@@ -30,7 +30,7 @@ module: arubaoss_aaa_accounting
 
 short_description: implements rest api for AAA Accounting configuration
 
-version_added: "2.4"
+version_added: "2.4.0"
 
 description:
     - "This implements rest apis which can be used to configure AAA Accounting"
