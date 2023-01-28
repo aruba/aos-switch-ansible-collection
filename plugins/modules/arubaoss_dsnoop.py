c93ed580577767e6b719bfcdc72040aebe01d7eb
Alexis La Goutte <alexis.lagoutte@gmail.com>
2023-01-28 20:39:27 +0000

    arubaoss_dsnoop: Fix sanity validates-modules
    arubaoss_dsnoop.py:0:0: doc-choices-do-not-match-spec: Argument 'dsnoop' in argument_spec defines choices as ([]) but documentation defines choices as ([True, False])
    arubaoss_dsnoop.py:0:0: doc-choices-do-not-match-spec: Argument 'is_dsnoop_option82_enabled' in argument_spec defines choices as ([]) but documentation defines choices as ([True, False])
    arubaoss_dsnoop.py:0:0: parameter-type-not-in-doc: Argument 'command' in argument_spec defines type as 'str' but documentation doesn't define type
    arubaoss_dsnoop.py:0:0: parameter-type-not-in-doc: Argument 'config' in argument_spec defines type as 'str' but documentation doesn't define type
    arubaoss_dsnoop.py:0:0: parameter-type-not-in-doc: Argument 'dsnoop' in argument_spec defines type as 'bool' but documentation doesn't define type
    arubaoss_dsnoop.py:0:0: parameter-type-not-in-doc: Argument 'is_dsnoop_option82_enabled' in argument_spec defines type as 'bool' but documentation doesn't define type
    arubaoss_dsnoop.py:0:0: parameter-type-not-in-doc: Argument 'remote_id' in argument_spec defines type as 'str' but documentation doesn't define type
    arubaoss_dsnoop.py:0:0: parameter-type-not-in-doc: Argument 'server_ip' in argument_spec defines type as 'str' but documentation doesn't define type
    arubaoss_dsnoop.py:0:0: parameter-type-not-in-doc: Argument 'untrusted_policy' in argument_spec defines type as 'str' but documentation doesn't define type

--- plugins/modules/arubaoss_dsnoop.py
+++ plugins/modules/arubaoss_dsnoop.py
@@ -41,37 +41,41 @@ options:
         choices: ["authorized_server","option_82", "dsnoop"]
         required: False
         default: "dsnoop"
+        type: str
     dsnoop:
         description: To enable or disable DHCP snooping.
-        choices: [True, False]
         required: False
         default: False
+        type: bool
     is_dsnoop_option82_enabled:
         description: To enable/disable adding option 82 relay information to DHCP client
                      packets that are forwarded on trusted ports
-        choices: [True, False]
         required: False
         default: True
+        type: bool
     remote_id:
         description: To select the address used as the Remote ID for option 82
         choices: ["DRI_MAC","DRI_SUBNET_IP", "DRI_MGMT_IP"]
         required: False
         default: "DRI_MAC"
+        type: str
     untrusted_policy:
         description: To set the policy for DHCP packets containing option 82 that are
                      received on untrusted ports
         choices: ["DUP_DROP","DUP_KEEP", "DUP_REPLACE"]
         required: False
         default: "DUP_DROP"
+        type: str
     server_ip:
         description: Add an authorized DHCP server address.
         required: False
         default: ""
+        type: str
     config:
         description: To configure or unconfigure the required command
         choices: ["create", "delete"]
         default: "create"
-
+        type: str
     host:
         description: >
             Specifies the DNS host name or address for connecting to the remote
