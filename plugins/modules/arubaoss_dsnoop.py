a347c5b31662316b3706145f75682b8746f4eb8c
Alexis La Goutte <alexis.lagoutte@gmail.com>
2023-02-05 20:21:40 +0000

    dsnoop: fix pylint
    ERROR: plugins/modules/arubaoss_dsnoop.py:317:40: E211: whitespace before '['
    ERROR: plugins/modules/arubaoss_dsnoop.py:318:5: E125: continuation line with same indent as next logical line
    ERROR: plugins/modules/arubaoss_dsnoop.py:318:41: E211: whitespace before '['
    ERROR: plugins/modules/arubaoss_dsnoop.py:327:10: E111: indentation is not a multiple of four
    ERROR: plugins/modules/arubaoss_dsnoop.py:327:10: E117: over-indented
    ERROR: plugins/modules/arubaoss_dsnoop.py:328:14: E111: indentation is not a multiple of four

--- plugins/modules/arubaoss_dsnoop.py
+++ plugins/modules/arubaoss_dsnoop.py
@@ -188,8 +188,8 @@ def authorized_server(module):
     if dsnoop_auth_server_config:
         check_config = module.from_json(to_text(dsnoop_auth_server_config))
         for ele in check_config['dsnoop_auth_server_element']:
-            if ele['authorized_server'] ['version'] == "IAV_IP_V4" and \
-                ele['authorized_server'] ['octets'] == params['server_ip']:
+            if ele['authorized_server']['version'] == "IAV_IP_V4" and \
+               ele['authorized_server']['octets'] == params['server_ip']:
                 match_ip = 'true'
 
     method = 'POST'
@@ -198,9 +198,9 @@ def authorized_server(module):
         url = url + '/' + params['server_ip']
 
     if match_ip == 'true':
-         if method == 'POST':
-             return {'msg': 'Specified authorized_server is already configured',
-                     'changed': False, 'failed': False}
+        if method == 'POST':
+            return {'msg': 'Specified authorized_server is already configured',
+                    'changed': False, 'failed': False}
     else:
         if method == 'DELETE':
             return {'msg': 'Specified authorized_server does not exists',
